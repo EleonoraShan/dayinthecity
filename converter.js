3d3ccc52029d111454ea8f53fad842005eeea35a
EleonoraShan <36446085+EleonoraShan@users.noreply.github.com>
2018-04-16 21:48:23 +0100

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,8 @@
+
+function CurrencyConverter(valNum) {
+  document.getElementById("outputEUR").innerHTML=valNum*1.15;
+}
+
+function CurrencyConverter2(valNum) {
+  document.getElementById("outputPLN").innerHTML=valNum*4.81;
+}
